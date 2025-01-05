50a3ac12688fbe12344b1f78e21673ac873708cf
razvi <razvan.uleia@gmail.com>
2025-01-05 20:25:18 +0200

    first commit

--- patients-records-service/src/main/java/com/example/project2/config/SecurityConfig.java
+++ patients-records-service/src/main/java/com/example/project2/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.example.project2.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,5 +22,13 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("{noop}admin123") // Use {noop} to disable password encoding for plain text passwords
+                .roles("ADMIN"); // Assign a role to the user
     }
 }
