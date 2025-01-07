16eb40a37cc809ba6c78782802bcbc69e95ad49f
razvi <razvan.uleia@gmail.com>
2025-01-07 18:34:46 +0200

    added unversioned files

---
+++ appointment-scheduling-service/src/main/java/com/example/appointmentschedulingservice/AppointmentSchedulingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.appointmentschedulingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentSchedulingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppointmentSchedulingServiceApplication.class, args);
+    }
+
+}
---
+++ appointment-scheduling-service/src/test/java/com/example/appointmentschedulingservice/AppointmentSchedulingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appointmentschedulingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentSchedulingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
