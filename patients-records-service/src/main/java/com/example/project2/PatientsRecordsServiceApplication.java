3a0351e769e8abbbab511346b58b98af726aaaec
razvi <razvan.uleia@gmail.com>
2025-01-07 23:11:18 +0200

    added eureka server

--- patients-records-service/src/main/java/com/example/project2/PatientsRecordsServiceApplication.java
+++ patients-records-service/src/main/java/com/example/project2/PatientsRecordsServiceApplication.java
@@ -8,10 +8,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication(scanBasePackages = "com.example.project2")
 @EntityScan(basePackages = {"com.api.project2.model.generated.patientsrecords"})
 @EnableJpaRepositories(basePackages = {"com.example.project2.repository"})
-public class Project2Application {
+public class PatientsRecordsServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Project2Application.class, args);
+        SpringApplication.run(PatientsRecordsServiceApplication.class, args);
 
     }
 
